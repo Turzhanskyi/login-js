5adfbb139d73a8b9f6f72bfd06c43990e1b66289
Turzhanskyi <turzhansky81@gmail.com>
2020-02-08 21:37:25 +0200

    Implemented notification

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ import UI from "./config/ui.config";
 import { validate } from "./helpers/validate";
 import { showInputError, removeInputError } from "./views/form";
 import { login } from "./services/auth.service";
+import { notify } from "./views/notifications";
 
 const { form, inputEmail, inputPassword } = UI;
 const inputs = [inputEmail, inputPassword];
@@ -31,5 +32,8 @@ async function onSubmit() {
   try {
     await login(inputEmail.value, inputPassword.value);
     form.reset();
-  } catch (err) {}
+    notify({ msg: "Login success", className: "alert-success" });
+  } catch (err) {
+    notify({ mas: "Login failed", className: "alert-danger" });
+  }
 }
